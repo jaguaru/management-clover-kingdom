ff10ed577415042765db42afe1f836f45809b1a6
jaguaru <jaguaru@outlook.com>
2024-05-31 15:16:30 -0600

    Adding Database class and configuration to database.py file

---
+++ magic_kingdom/app/database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+class Database:
+    """
+    Class to configure and manage the database connection.
+    """
+    def __init__(self, db_url):
+        """
+        Initialize the Database class with the database URL.
+
+        """
+        self.db_url = db_url
+        self.engine = create_engine(self.db_url)
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+        self.Base = declarative_base()
+    
+    def get_session(self):
+        """
+        Get a new database session.
+        """
+        return self.SessionLocal()
+
+
+# Database configuration
+DATABASE_URL = "postgresql://jaguaru:Jaguar12345@localhost/magic_academy_db"
+
+db = Database(DATABASE_URL)
