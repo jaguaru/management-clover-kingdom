f952dc5b37e49d04a23bc06348db64020d2ae993
jaguaru <jaguaru@outlook.com>
2024-05-31 20:10:36 -0600

    Adding create_solicitud to crud.py file

--- magic_kingdom/app/student_endpoints.py
+++ magic_kingdom/app/student_endpoints.py
@@ -1,4 +1,12 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from .. import crud, schemas
+from ..database import get_db
 
 
 router = APIRouter()
+
+
+@router.post("/solicitud/", response_model=schemas.Solicitud)
+def create_solicitud(solicitud: schemas.SolicitudCreate, db: Session = Depends(get_db)):
+    return crud.create_solicitud(db=db, solicitud=solicitud)
