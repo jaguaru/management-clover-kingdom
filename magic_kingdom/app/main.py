7a7a647a1eb8df2159ff82d541bd55c5e6e9f4c2
jaguaru <jaguaru@outlook.com>
2024-05-31 19:19:54 -0600

    Creating executable script main.py file

--- magic_kingdom/app/main.py
+++ magic_kingdom/app/main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
 
 
 app = FastAPI()
+
+
+if __name__ == "__main__":
+    """
+    This block ensures that the script runs only when executed directly,
+    not when imported as a module
+    """
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
