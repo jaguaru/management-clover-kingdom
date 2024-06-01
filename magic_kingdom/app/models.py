5caebc109647403fb2755a158c6c5298294ece46
jaguaru <jaguaru@outlook.com>
2024-06-01 12:54:24 -0600

    Adding Grimorio class and relationship in models.py file

--- magic_kingdom/app/models.py
+++ magic_kingdom/app/models.py
@@ -16,3 +16,22 @@ class Solicitud(db.Base):
     edad = Column(Integer)
     afinidad_magica = Column(Enum('Oscuridad', 'Luz', 'Fuego', 'Agua', 'Viento', 'Tierra', name='afinidad_magica'))
     estatus = Column(String, default="pendiente")
+
+
+class Grimorio(Base):
+    """
+    Represents the Grimorio table in the database.
+    """
+
+    __tablename__ = "grimorios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    tipo_trebol = Column(String, index=True)
+    rareza = Column(String, index=True)
+    magia = Column(String, index=True)
+    escudo = Column(Integer, index=True)
+    solicitud_id = Column(Integer, ForeignKey('solicitudes.id'))
+
+    solicitud = relationship("Solicitud", back_populates="grimorios")
+
+Solicitud.grimorios = relationship("Grimorio", back_populates="solicitud")
