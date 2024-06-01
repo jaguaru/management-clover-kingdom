125a6bacf885a7fe1075263c2068a6c587d89891
jaguaru <jaguaru@outlook.com>
2024-06-01 00:20:57 -0600

    Adding get_solicitud_by_identificacion in crud.py file

--- magic_kingdom/app/crud.py
+++ magic_kingdom/app/crud.py
@@ -2,6 +2,10 @@ from sqlalchemy.orm import Session
 from . import models, schema
 
 
+def get_solicitud_by_identificacion(db: Session, identificacion: str):
+    return db.query(models.Solicitud).filter(models.Solicitud.identificacion == identificacion).first()
+
+
 def create_solicitud(db: Session, solicitud: schema.SolicitudCreate):
     db_solicitud = models.Solicitud(**solicitud.dict())
     db.add(db_solicitud)
