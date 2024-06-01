974445809acec79c548521b02c5a2d8f47838d62
jaguaru <jaguaru@outlook.com>
2024-06-01 00:19:36 -0600

    Adding try except and jsonresponse messages student_endpoints.py file

--- magic_kingdom/app/student_endpoints.py
+++ magic_kingdom/app/student_endpoints.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from . import crud, schema
 from .database import get_db
@@ -9,4 +10,19 @@ router = APIRouter()
 
 @router.post("/solicitud/", response_model=schema.Solicitud)
 def create_solicitud(solicitud: schema.SolicitudCreate, db: Session = Depends(get_db)):
-    return crud.create_solicitud(db=db, solicitud=solicitud)
+
+    try:
+        db_solicitud = crud.create_solicitud(db=db, solicitud=solicitud)
+        return JSONResponse(
+            status_code=201,
+            content={
+                "message": "Request added successfully!", 
+                "identificacion": db_solicitud.identificacion
+            }
+        )
+
+    except Exception as _except:
+        return JSONResponse(
+            status_code=400,
+            content={"message": "Could not add this request!", "error": _except}
+        )
