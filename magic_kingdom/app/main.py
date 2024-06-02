b9ccb087d311f44dea7d89a0b78db0e41cdb7b09
jaguaru <jaguaru@outlook.com>
2024-06-01 19:12:55 -0600

    Adding create_app to main.py file

--- magic_kingdom/app/main.py
+++ magic_kingdom/app/main.py
@@ -3,14 +3,20 @@ import uvicorn
 from student_endpoints import router as solicitud_router
 
 
-app = FastAPI()
+def create_app():
+    """
+    This function creates and configures the FastAPI application.
+    """
+    app = FastAPI()
+    app.include_router(solicitud_router, prefix="/api")
+    return app
 
-app.include_router(solicitud_router, prefix="/api")
+app = create_app()
 
 
 if __name__ == "__main__":
     """
     This block ensures that the script runs only when executed directly,
-    not when imported as a module
+    not when imported as a module.
     """
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
